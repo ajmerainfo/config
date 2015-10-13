9ff9a88c37f19144ce223f99b3f311b098bfb55a
Keyur Ajmera <keyur.ajmera@spinxdigital.com>
2015-10-12 17:34:22 -0700

    Init commit for test package

---
+++ src/Config.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Config.php
+ *
+ * Created by Keyur Ajmera.
+ * User: Keyur Ajmera
+ * Date: 10/12/2015
+ * Time: 5:09 PM
+ *
+ * Read .config file and load all variables
+ */
+
+namespace Ajmerainfo;
+
+
+class Config
+{
+    public function Test(){
+        echo "Hello from first Package";
+    }
+}
